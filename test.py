91ce35f67ea446c23fa925d4c6bf23bc6943bec4
LeoQ <cnmwozhendebuzhidao@gmail.com>
2017-05-08 13:44:54 +0800

    first edition

---
+++ test.py
@@ -0,0 +1,4 @@
+from weixin import miaomiaomiao
+for i in range(10):
+    ans = miaomiaomiao('喵喵喵')
+    print(ans)
---
+++ weixin.py
@@ -0,0 +1,47 @@
+# 导入模块
+from wxpy import *
+import random
+# 初始化机器人，扫码登陆
+
+def miaomiaomiao(content):
+    print(content)
+    emoji_list = ['(゜-゜)','(=`ｪ´=；)ゞ','(;´༎ຶД༎ຶ`)','⁄(⁄⁄•⁄ω⁄•⁄⁄)⁄','(´･_･`)','(｀∀´)','(:3[▓▓▓]','（╯‵□′）╯︵┴─┴','₍•͈˽•͈₎','∠( ᐛ 」∠)＿','(*ﾉωﾉ)','ヾ(*>∀＜*) ','(ฅωฅ*)','<(￣︶￣)/ ','(´⊙ω⊙`)','(๑•̀ω•́๑)','(≧∀≦)♪','(*/∇＼*)','(｡･ω･｡)ﾉ♡','( • ̀ω•́ )✧','(☆ω☆)','╮(￣⊿￣")╭','(′へ`、)','(ﾟﾛ ﾟﾉ)ﾉ','(*｀▽´*)','Ծ‸Ծ']
+    if '喵喵' in content or '猫猫' in content:
+        sig_choices = ['？','！','~','~~~','？？？','？！','！！！']
+        miao_count = random.randrange(3,15)
+        reply_list = ['喵'] * miao_count
+        sig_count = random.randrange(0,miao_count-2)
+        emoji_count = random.randrange(0,2)
+        if emoji_count == 0:
+            emoji = ''
+        else:
+            emoji = random.choice(emoji_list)
+        reply_list += [emoji]
+        for i in range(sig_count+1):
+            reply_list += [random.choice(sig_choices)]
+        random.shuffle(reply_list)
+        return ''.join(reply_list)
+ 
+if __name__ == '__main__':
+    bot = Bot(console_qr=True)
+    bot.file_helper.send('微信机器人已成功登录')
+    listening_group = ensure_one(bot.groups().search('test_group'))
+    fans_group = ensure_one(bot.groups().search('同好会'))
+    @bot.register(listening_group)
+    def test_group_auto(msg):
+        print(msg.type)
+        reply = None
+        if msg.type == 'Text':
+            reply = miaomiaomiao(msg.text)
+        if reply:
+            listening_group.send(reply)
+    @bot.register(fans_group)
+    def test_group_auto(msg):
+        print(msg.type)
+        reply = None
+        if msg.type == 'Text':
+            reply = miaomiaomiao(msg.text)
+        if reply:
+            fans_group.send(reply)
+    embed()
+    
